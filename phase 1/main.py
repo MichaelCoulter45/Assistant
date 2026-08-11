5545e5cf90d92228d34aa59ed5e60f70659bbd7d
Michael Coulter <coulte12@go.stockton.edu>
2026-08-11 10:04:50 -0400

    v0.01 - opening google chrome via terminal command

---
+++ phase 1/main.py
@@ -0,0 +1,42 @@
+# main.py
+# imports
+import subprocess
+
+# Paths
+chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+# commands
+def commands(user_input):
+    if user_input.strip().lower() == "open chrome":
+        print(f"Executing: {user_input}")
+        subprocess.run([chrome_path])
+    else:
+        print(f"Error. Unknown command.")
+        
+
+def ask_for_command():
+        print(f"What can I do for you today?")
+        commands(user_input=input())
+        
+
+
+# main()
+def main():
+    print(f"\nHello!")
+    ask_for_command()
+    
+    #end of main()
+    print()
+
+
+
+
+
+
+
+
+
+
+
+# if name == main
+if __name__ == "__main__":
+    main()
