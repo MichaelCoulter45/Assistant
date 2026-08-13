25a2767aa53a2e02d9b407a4290a01de42084281
Michael Coulter <coulte12@go.stockton.edu>
2026-08-13 10:32:19 -0400

    Update main.py
    Adding a little bit of dynamic matching for the one command.

--- phase 1/main.py
+++ phase 1/main.py
@@ -8,8 +8,9 @@ import subprocess
 
 
 """ ## Current Known Bugs ##
-1. user input isn't parsing correctly. Check the two command functions.
+1. XX user input isn't parsing correctly. Check the two command functions.
 2. Script crashes when inputting an incorrect command or 'q' for missing values.
+3. The only command now opens chrome despite naming firefox or others in objects[]
 """
 
 
@@ -32,19 +33,28 @@ def toggle_active():
     active = not active
 
 
-def process_user_input(user_verb, user_object, verbs=verbs, objects=objects):
-    
+def process_user_input(user_input, verbs=verbs, objects=objects):
+    """ 
+    The point of this function is to roll through the user's command to find the verb and object to dynamically
+    call the correct command instead of hard-coding each possible command with if-statements.
+    * This is currently assuming the user's command is specifically two words.
+    """
+    # The user's command is already normalized and split, so we can work with it now.
+    for _ in range(len(verbs)):
+        if user_input[0] == verbs[_]:
+            user_verb = user_input[0]
+        if user_input[1] == objects[_]:
+            user_object = user_input[1]
+    # end of function
     return user_verb, user_object
 
 
 # commands
 def commands(user_verb, user_object):
     if user_verb and user_object:
-        user_verb = user_verb.strip().lower()
-        user_object = user_object.strip().lower()
         
     
-        if user_verb == "open" and user_object == "chrome":
+        if user_verb in verbs and user_object in objects:
             print(f"Executing: '{user_verb} {user_object}'\n")
             subprocess.Popen([PATH_CHROME])
         elif user_verb == "q":
@@ -58,15 +68,8 @@ def commands(user_verb, user_object):
 def ask_for_command():
         print(f"What can I do for you today? [Enter 'Q' to quit.]")
         user_input = input()
-        user_input = user_input.split()
-        for _ in range(len(user_input)):
-        # for _ in range(len(verbs)):
-            if user_input[0] == verbs[_]:
-                user_verb = user_input[0]
-        # for _ in objects:
-            if user_input[1] == objects[_]:
-                user_object = user_input[1]
-        
+        user_input = user_input.strip().lower().split()
+        user_verb, user_object = process_user_input(user_input)
         commands(user_verb, user_object)
         
 
@@ -76,7 +79,7 @@ def main():
     print(f"\nHello!")
     while active:
         ask_for_command()
-    print(f"Goodbye!")
+    print(f"\nGoodbye!")
     
     #end of main()
     print()
