d53c38f93da45716dea01dba3128fe1beb0a657e
Michael Coulter <coulte12@go.stockton.edu>
2026-08-20 18:59:28 -0400

    Update main.py

--- phase 1/main.py
+++ phase 1/main.py
@@ -2,6 +2,8 @@
 import subprocess
 import shutil
 import os
+from pathlib import Path
+
 
 
 """ ## Current Known Bugs ##
@@ -10,6 +12,14 @@ import os
 3. XX The only command now opens chrome despite naming firefox or others in objects[]
 4. Currently only hard coded objects are supported. Fix it to make dynamic.
 """
+###
+""" 
+Things to do:
+1. Make it so only the first word in user_input is the verb, and the rest is the object.
+2. Update process_user_input(). 
+"""
+
+
 
 hotkey_quit = 'q'
 
@@ -21,7 +31,7 @@ PATH_CHROME = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 
 verbs = ["open", "start", "launch"]
-objects = ["google", "chrome", "firefox", "brave", "opera gx", "opera"]
+# objects = ["google", "chrome", "firefox", "brave", "opera gx", "opera"]
 
 ###############
 def toggle_active():
@@ -31,24 +41,28 @@ def toggle_active():
 
 
 # Finding Apps and opening them.
-def process_user_input(user_input, verbs=verbs, objects=objects):
+def process_user_input(user_input, verbs=verbs):
     """ 
     The point of this function is to roll through the user's command to find the verb and object to dynamically
     call the correct command instead of hard-coding each possible command with if-statements.
     * This is currently assuming the user's command is specifically two words.
     """
+    user_object = ""
     # The user's command is already normalized and split, so we can work with it now.
     if len(user_input) >= 2:
         if user_input[0] in verbs:
             user_verb = user_input[0]
-        if user_input[1] in objects:
-            user_object = user_input[1]
+        
+        # All other words in user's input is the object.
+        for word in user_input:
+            user_object += word + " "
+        
     else:
         print(f"Can you expand on {user_input}?\n")
         user_verb, user_object = None, None
         return user_verb, user_object
     # end of function
-    return user_verb, user_object
+    return user_verb, user_object[len(user_verb):].strip()
 
 
 def find_application(user_object): #### Currently searches entire drive. Takes too long...
@@ -57,10 +71,33 @@ def find_application(user_object): #### Currently searches entire drive. Takes t
     if path:
         return path
     
+    home_dir = Path.home()
+    user_object = user_object + ".exe"
+    
+    # Search likely locations first: \Program Files, \Program Files (x86), and ...\Local
+    # C:\Program Files
+    for root, dirs, files in os.walk(r"C:\Program Files"): 
+        if user_object in files:
+            path = os.path.join(root, user_object)
+            return path
+    
+    # C:\Program Files (x86)
+    for root, dirs, files in os.walk(r"C:\Program Files (x86)"): 
+        if user_object in files:
+            path = os.path.join(root, user_object)
+            return path
+    
+    # ...\Appdata\Local
+    for root, dirs, files in os.walk({str(home_dir)}+"\AppData\Local"): # <-------------------- This line
+        if user_object in files:
+            path = os.path.join(root, user_object)
+            return path
+    
     # Last Resort: Walk the whole C:\ drive to find the target.
     for root, dirs, files in os.walk(r"C:\\"):
-        if (user_object + ".exe") in files:
-            path = os.path.join(root, user_object + ".exe")
+        if user_object in files:
+            path = os.path.join(root, user_object)
+            return path
     return path
 
 
@@ -79,15 +116,15 @@ def ask_for_command():
 
 def commands(user_verb, user_object):
     if user_verb and user_object:
-        if user_verb in verbs and user_object == "chrome":
-            print(f"Executing: '{user_verb} {user_object}'\n")
-            target_app = find_application(user_object)
-            if target_app:
-                subprocess.Popen([target_app])
-            else:
-                print(f"Cannot find {user_object}.\n")
+        # if user_verb in verbs and user_object == "chrome":
+        print(f"Executing: '{user_verb} {user_object}'\n")
+        target_app = find_application(user_object)
+        if target_app:
+            subprocess.Popen([target_app])
         else:
-            print(f"Error: Unknown command: '{user_verb} {user_object}'\n")
+            print(f"Cannot find {user_object}.\n")
+    else:
+        print(f"Error: Unknown command: '{user_verb} {user_object}'\n")
 ####################
 # main()
 def main():
@@ -96,8 +133,8 @@ def main():
         ask_for_command()
     print(f"\nGoodbye!\n")
     # Debugging
-    print("Shutil: ", shutil.which("chrome.exe"))
-    print("Shutil: ", shutil.which("python.exe"))
+    # print("Shutil: ", shutil.which("chrome.exe"))
+    # print("Shutil: ", shutil.which("python.exe"))
 
     #end of main()
 if __name__ == "__main__":
